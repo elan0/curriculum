fc12210c5d064b5fb599a6682e4b6bc3050b128f
elan0 <gasmaskrobotomi@gmail.com>
2021-04-15 20:16:43 +0900

    MVC

--- JavaStudy/IFTEST/src/Test01/Sample.java
+++ JavaStudy/IFTEST/src/Test01/Sample.java
@@ -1,25 +1,24 @@
 package Test01;
 
 public class Sample {
-	public static void main(String[] args) {
-		int[] data = {34,31};
+	public static void main(String[] args) throws Exception {
+
+		int[] data = { 3, 1, 2, 7, 5 };
+
+		for (int i = 0; i < 20; i++) {
+			System.out.println(i+"回目の繰り返し");
+
+			/** この下は無視 */
+			try {
+
+				Thread.sleep(1000);
 
 
-		System.out.print("前＝"+data[0]+",");
-		System.out.println(data[1]);
 
-		//ソート
-		if(data[0] > data[1]){
-			//box =34 data[0]=34 data[1]=31
-			int box = data[0];
-			//box =34 data[0]=31 data[1]=31
-			data[0] = data[1];
-			//box =34 data[0]=31 data[1]=34
-			data[1] = box;
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
-
-
-		System.out.print("後＝"+data[0]+",");
-		System.out.println(data[1]);
 	}
 }
